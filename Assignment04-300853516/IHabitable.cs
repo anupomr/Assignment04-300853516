f7b286b330362c495481abcc17d121c322916248
Anupom Roy <aroy34@my.centennialcollege.ca>
2017-07-25 12:06:19 -0400

    IHasMoons interface Created

---
+++ Assignment04-300853516/IHabitable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Anupom Roy
+ * Date: July 25, 2017
+ * Description: This is an Interface that defines  Habitable
+ * Version: 0.1 - Created IHasMoons interface
+ */
+namespace Assignment04_300853516
+{
+    public interface IHabitable
+    {
+        // Public Method 
+        bool Habitable();
+    }
+}
