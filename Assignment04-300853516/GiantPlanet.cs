8a8732b8a68d345df6db42a458ae83e4b981df05
Anupom Roy <aroy34@my.centennialcollege.ca>
2017-07-25 13:22:27 -0400

    GiantPlanet class Modified with Comment

--- Assignment04-300853516/GiantPlanet.cs
+++ Assignment04-300853516/GiantPlanet.cs
@@ -7,18 +7,28 @@ using System.Text;
  * Date: July 25, 2017
  * Description: This is the GiantPlanet class which inherits from the Planet class
  * and implements the IHasMoons, IHasRings
- * Version: 0.1 -GiantPlanet class Created.
+ * Version: 0.2 -GiantPlanet class Modified with Comment.
  */
 namespace Assignment04_300853516
 {
     public class GiantPlanet: Planet, IHasMoons, IHasRings
     {
+        // PRIVATE FIELDS
         private string _type;
+       /// <summary>
+       /// Constractor
+       /// </summary>
+       /// <param name="type"></param>
+       /// <param name="name"></param>
+       /// <param name="diameter"></param>
+       /// <param name="mass"></param>
         public GiantPlanet(string type, string name, double diameter, double mass)
             :base(name,diameter,mass)
         {
             this._type = type;
         }
+
+        // PUBLIC METHODS
         public bool HasMoons()
         {
             return (MoonCount > 0);
